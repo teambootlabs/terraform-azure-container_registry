18a291efa0b893f4ee9215f0980a88924ff2d390
suribabu8 <suribabu@bootlabtech.com>
2022-04-16 11:57:33 +0530

    locks

--- main.tf
+++ main.tf
@@ -7,6 +7,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "example-resources"
   location = var.location
 }
+resource "azurerm_management_lock" "resource-group-level" {
+  name       = "resource-group-level"
+  scope      = azurerm_resource_group.example.id
+  lock_level = "ReadOnly"
+  notes      = "This Resource Group is Read-Only"
+}
 
 resource "azurerm_container_registry" "acr" {
   name                          = "containerRegistry1"
