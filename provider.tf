7bc560facb672d978e9ac6bd79c6d349bc068a5a
suribabu8 <suribabu@bootlabtech.com>
2022-04-16 09:25:30 +0530

    container registry

--- provider.tf
+++ provider.tf
@@ -1,10 +1,18 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  /*whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider*/
+  subscription_id = "c3a419d3-951d-4355-9cf3-dc49da58a664"
+  client_id       = "e9bef17b-53f3-4954-b1c0-3d1b6082ef56"
+  client_secret   = "~mW7Q~J-APaSEFV5Es2OswEy~ZZXSJ4Wjtlsn"
+  tenant_id       = "b45ba00e-ef83-40b4-ad06-9d7078cdbc30"
+
+  features {}
+}
 terraform {
-  experiments = [module_variable_optional_attrs]
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 2.59.0"
+      version = "2.98.0"
     }
   }
-  required_version = ">= 0.13"
 }
--- variable.tf
+++ variable.tf
@@ -1,138 +1,48 @@
-variable "create_resource_group" {
-  description = "Whether to create resource group and use it for all networking resources"
-  default     = false
+variable "location" {
+  description = "(Optional) The location in which the resources will be created."
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "azurerm_container_registry" {
+  description = "azure container registry"
+  type        = string
+  default     = "containerRegistry1"
 }
 
 variable "resource_group_name" {
-  description = "A container that holds related resources for an Azure solution"
-  default     = ""
+  description = "Optional globally unique per datacenter region domain name label to apply to each public ip address. e.g. thisvar.varlocation.cloudapp.azure.com where you specify only thisvar here. This is an array of names which will pair up sequentially to the number of public ips defined in var.nb_public_ip. One name or empty string is required for every public ip. If no public ip is desired, then set this to an array with a single empty string."
+  type        = string
+  default     = "apr-11-2022"
 }
 
-variable "location" {
-  description = "The location/region to keep all your network resources. To get the list of all locations with table format from azure cli, run 'az account list-locations -o table'"
-  default     = ""
+variable "sku" {
+  description = "Same as ssh_key, but allows for setting multiple public keys. Set your first key in ssh_key, and the extras here."
+  type        = string
+  default     = "premium"
 }
-
-variable "container_registry_config" {
-  description = "Manages an Azure Container Registry"
-  type = object({
-    name                          = string
-    admin_enabled                 = optional(bool)
-    sku                           = optional(string)
-    public_network_access_enabled = optional(bool)
-    quarantine_policy_enabled     = optional(bool)
-    zone_redundancy_enabled       = optional(bool)
-  })
-}
-
-variable "georeplications" {
-  description = "A list of Azure locations where the container registry should be geo-replicated"
-  type = list(object({
-    location                = string
-    zone_redundancy_enabled = optional(bool)
-  }))
-  default = []
-}
-
-variable "network_rule_set" { # change this to match actual objects
-  description = "Manage network rules for Azure Container Registries"
-  type = object({
-    default_action = optional(string)
-    ip_rule = optional(list(object({
-      ip_range = string
-    })))
-    virtual_network = optional(list(object({
-      subnet_id = string
-    })))
-  })
-  default = null
-}
-
-variable "retention_policy" {
-  description = "Set a retention policy for untagged manifests"
-  type = object({
-    days    = optional(number)
-    enabled = optional(bool)
-  })
-  default = null
-}
-
-variable "enable_content_trust" {
-  description = "Boolean value to enable or disable Content trust in Azure Container Registry"
+variable "admin_enabled" {
+  type        = bool
+  description = "Delete datadisk when machine is terminated."
   default     = false
 }
 
-variable "identity_ids" {
-  description = "Specifies a list of user managed identity ids to be assigned. This is required when `type` is set to `UserAssigned` or `SystemAssigned, UserAssigned`"
-  default     = null
+variable "zone_redundancy_enabled" {
+  type        = bool
+  description = "Delete datadisk when machine is terminated."
+  default     = true
 }
-
-variable "encryption" {
-  description = "Encrypt registry using a customer-managed key"
-  type = object({
-    key_vault_key_id   = string
-    identity_client_id = string
-  })
-  default = null
-}
-
-variable "scope_map" {
-  description = "Manages an Azure Container Registry scope map. Scope Maps are a preview feature only available in Premium SKU Container registries."
-  type = map(object({
-    actions = list(string)
-  }))
-  default = null
-}
-
-variable "container_registry_webhooks" {
-  description = "Manages an Azure Container Registry Webhook"
-  type = map(object({
-    service_uri    = string
-    actions        = list(string)
-    status         = optional(string)
-    scope          = string
-    custom_headers = map(string)
-  }))
-  default = null
-}
-
-variable "enable_private_endpoint" {
-  description = "Manages a Private Endpoint to Azure Container Registry"
-  default     = false
-}
-
-variable "virtual_network_name" {
-  description = "The name of the virtual network"
-  default     = ""
-}
-
-variable "existing_private_dns_zone" {
-  description = "Name of the existing private DNS zone"
-  default     = null
-}
-
-variable "private_subnet_address_prefix" {
-  description = "The name of the subnet for private endpoints"
-  default     = null
-}
-
-variable "log_analytics_workspace_name" {
-  description = "The name of log analytics workspace name"
-  default     = null
-}
-
-variable "storage_account_name" {
-  description = "The name of the hub storage account to store logs"
-  default     = null
-}
-
-variable "acr_diag_logs" {
-  description = "Application Gateway Monitoring Category details for Azure Diagnostic setting"
-  default     = ["ContainerRegistryRepositoryEvents", "ContainerRegistryLoginEvents"]
-}
-
 variable "tags" {
-  description = "A map of tags to add to all resources"
   type        = map(string)
-  default     = {}
+  description = "A map of the tags to use on the resources that are deployed with this module."
+
+  default = {
+    source = "terraform"
+  }
 }
+variable "public_network_access_enabled" {
+  type        = bool
+  description = "Delete datadisk when machine is terminated."
+  default     = false
+}
+
